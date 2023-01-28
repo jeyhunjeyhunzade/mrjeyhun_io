952cf8bbc2b7f010c7dc0c1778b09b7a569f41ca
MrJeyhun <ceyhun.zade1998@gmail.com>
2023-01-29 01:03:44 +0400

    initialized layout for the project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,32 +1,42 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+    children?: ReactNode;
+    title?: string;
+};
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+    <div>
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Grid
+            templateAreas={`"header"
+                  "main"
+                  "footer"`}
+            gridTemplateRows={'5.6rem auto 8.7rem'}
+            // gridTemplateColumns={'150px 1fr'}
+            h="100vh"
+            gap="1"
+            color="blackAlpha.700"
+            fontWeight="bold"
+        >
+            <GridItem bg="orange.300" area={'header'}>
+                Header
+            </GridItem>
+            <GridItem bg="green.300" area={'main'}>
+                {children}
+            </GridItem>
+            <GridItem sx={{ bottom: '0' }} bg="blue.300" area={'footer'}>
+                <span>I'm here to stay (Footer)</span>
+            </GridItem>
+        </Grid>
+    </div>
+);
 
-export default Layout
+export default Layout;
